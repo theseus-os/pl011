2c2cec84ca9653efa9e370460e7e0e1c94140080
NathanRoyer <nathan.royer.pro@gmail.com>
2023-01-24 17:57:56 +0100

    Allow the base address to be specified at runtime

--- src/lib.rs
+++ src/lib.rs
@@ -92,32 +92,15 @@ pub struct Error;
 /// // build a driver for UART1
 /// let mut uart = pl011_qemu::PL011::new(pl011_qemu::UART1::take().unwrap());
 /// ```
-pub struct PL011<UART> {
-    /// owned copy of the underlying uart registers.
-    ///
-    ///Since they are moved into this struct, no one else can access them after the driver is initialized
-    pub regs: UART,
-    rx: Rx<UART>,
-    tx: Tx<UART>,
+pub struct PL011 {
+    regs: &'static mut PL011_Regs,
 }
 
-/// Represents read half of UART.
-pub struct Rx<UART> {
-    _uart: PhantomData<UART>,
-}
-
-/// Represents write half of UART
-pub struct Tx<UART> {
-    _uart: PhantomData<UART>,
-}
-
-impl<T> Rx<T>
-where
-    T: detail::ConstRegBlockPtr<PL011_Regs>,
-{
+/// RX methods
+impl PL011 {
     /// Is the receive-buffer-empty flag clear?
     pub fn has_incoming_data(&self) -> bool {
-        let uartfr = unsafe { (*T::ptr()).uartfr.read() };
+        let uartfr = unsafe { (*self.regs).uartfr.read() };
         uartfr & 0x10 == 0
     }
 
@@ -128,55 +111,47 @@ where
         // loop while RXFE is set
         while !self.has_incoming_data() {}
         // read the data register. Atomic read is side effect free
-        let data = unsafe { (*T::ptr()).uartdr.read() & 0xff };
+        let data = unsafe { (*self.regs).uartdr.read() & 0xff };
         data as u8
     }
 }
 
-impl<T> Tx<T>
-where
-    T: detail::ConstRegBlockPtr<PL011_Regs>,
-{
+/// TX methods
+impl PL011 {
     /// writes a single byte out the uart
     ///
     /// spins until space is available in the fifo
     pub fn write_byte(&self, data: u8) {
         while !self.is_writeable() {}
-        unsafe { (*T::ptr()).uartdr.write(data as u32) };
+        unsafe { (*self.regs).uartdr.write(data as u32) };
     }
 
     /// Is the transmit-buffer-full flag clear?
     pub fn is_writeable(&self) -> bool {
-        let uartfr = unsafe { (*T::ptr()).uartfr.read() };
+        let uartfr = unsafe { (*self.regs).uartfr.read() };
         uartfr & 0x20 == 0
     }
 }
 
-impl<T> serial::Read<u8> for Rx<T>
-where
-    T: detail::ConstRegBlockPtr<PL011_Regs>,
-{
+impl serial::Read<u8> for PL011 {
     type Error = Error;
 
     fn read(&mut self) -> nb::Result<u8, Self::Error> {
         // if RXFE is set (rx fifo is empty)
         if self.has_incoming_data() {
-            Ok(unsafe { (*T::ptr()).uartdr.read() & 0xff } as u8)
+            Ok(unsafe { (*self.regs).uartdr.read() & 0xff } as u8)
         } else {
             Err(nb::Error::WouldBlock)
         }
     }
 }
 
-impl<T> serial::Write<u8> for Tx<T>
-where
-    T: detail::ConstRegBlockPtr<PL011_Regs>,
-{
+impl serial::Write<u8> for PL011 {
     type Error = Error;
 
     fn write(&mut self, word: u8) -> nb::Result<(), Self::Error> {
         self.flush()?;
-        unsafe { (*T::ptr()).uartdr.write(word as u32) };
+        unsafe { (*self.regs).uartdr.write(word as u32) };
         Ok(())
     }
 
@@ -189,10 +164,7 @@ where
     }
 }
 
-impl<T> fmt::Write for Tx<T>
-where
-    Tx<T>: serial::Write<u8>,
-{
+impl fmt::Write for PL011 {
     fn write_str(&mut self, s: &str) -> fmt::Result {
         use embedded_hal::serial::Write;
         for b in s.as_bytes().iter() {
@@ -204,155 +176,11 @@ where
     }
 }
 
-impl<T> PL011<T>
-where
-    T: detail::ConstRegBlockPtr<PL011_Regs>,
-{
+/// Generic methods
+impl PL011 {
     /// Initialize a UART driver. Needs a UART struct to be passed in
-    pub fn new(uart: T) -> Self {
-        let pl011 = PL011 {
-            regs: uart,
-            tx: Tx { _uart: PhantomData },
-            rx: Rx { _uart: PhantomData },
-        };
-        pl011
-    }
-
-    /// splits a single PL011 uart object into separate Rx and Tx streams.
-    ///
-    /// Useful when you want to separate the two different halves of
-    /// the UART and use them in different parts of the application
-    ///
-    /// Note that the Rx and Tx structs do not contain a reference to
-    /// the underlying UART, this is because once you split the UART
-    /// in half, it is no longer possible to interact with the uart
-    /// hardware directly, since the other half (rx or tx) might be
-    /// using it at the same time.
-    pub fn split(self) -> (Tx<T>, Rx<T>) {
-        (self.tx, self.rx)
-    }
-
-    /// writes a single byte out the uart
-    ///
-    /// spins until space is available in the fifo
-    pub fn write_byte(&self, data: u8) {
-        self.tx.write_byte(data);
-    }
-
-    /// checks if the receive buffer has available data
-    pub fn has_incoming_data(&self) -> bool {
-        self.rx.has_incoming_data()
-    }
-
-    /// checks that the transmit buffer is not full
-    pub fn is_writeable(&self) -> bool {
-        self.tx.is_writeable()
-    }
-
-    /// reads a single byte out the uart
-    ///
-    /// spins until a byte is available in the fifo
-    pub fn read_byte(&self) -> u8 {
-        self.rx.read_byte()
-    }
-}
-
-impl<T> fmt::Write for PL011<T>
-where
-    Tx<T>: fmt::Write,
-{
-    fn write_str(&mut self, s: &str) -> fmt::Result {
-        self.tx.write_str(s)
-    }
-}
-
-impl<T> serial::Read<u8> for PL011<T>
-where
-    Rx<T>: serial::Read<u8, Error = Error>,
-{
-    type Error = Error;
-    fn read(&mut self) -> nb::Result<u8, Self::Error> {
-        self.rx.read()
-    }
-}
-
-impl<T> serial::Write<u8> for PL011<T>
-where
-    Tx<T>: serial::Write<u8, Error = Error>,
-{
-    type Error = Error;
-
-    fn write(&mut self, word: u8) -> nb::Result<(), Self::Error> {
-        self.tx.write(word)
-    }
-
-    fn flush(&mut self) -> nb::Result<(), Self::Error> {
-        self.tx.flush()
-    }
-}
-
-macro_rules! create_uart {
-    (
-        $(#[$attr:meta])* struct $uart:ident,
-        $global:ident,
-        $addr:literal
-    ) => {
-        $(#[$attr])*
-            pub struct $uart;
-        impl detail::ConstRegBlockPtr<PL011_Regs> for $uart {
-            /// returns a pointer to the register block
-            fn ptr() -> *const PL011_Regs {
-                $addr as *const _
-            }
-        }
-        unsafe impl Send for $uart {}
-        impl Deref for $uart {
-            type Target = PL011_Regs;
-            #[inline(always)]
-            fn deref(&self) -> &Self::Target {
-                use crate::detail::ConstRegBlockPtr;
-                unsafe {&* $uart::ptr()}
-            }
-        }
-        static mut $global : bool = false;
-        impl $uart {
-            /// takes the UART HW
-            ///
-            /// Can only be taken once. If taken again returns None
-            pub fn take() -> Option<Self> {
-                if unsafe {$global} {
-                    None
-                } else {
-                    Some(unsafe {$uart::steal()})
-                }
-            }
-            /// Unsafe function to steal hardware resources.
-            ///
-            /// Should not be needed if properly passing around the uart
-            pub unsafe fn steal() -> Self {
-                $global = true;
-                $uart
-            }
-        }
-    }
-}
-create_uart!(
-    /// Hardware Singleton for UART1 
-    struct UART1,
-    UART1_TAKEN, 0x0900_0000);
-
-/// miscellaneous internal traits
-pub mod detail {
-    /// Trait to indicate that the following type always points to a fixed hardware resource
-    ///
-    /// This trait is used since there is a uniqe type for each UART
-    /// hardware component. If you have access to that type, you can
-    /// access the registers of its underlying hardware through the
-    /// type
-    pub trait ConstRegBlockPtr<R> {
-        /// returns a pointer to the fixed address of the underlying hardware resource  
-        fn ptr() -> *const R
-        where
-            Self: Sized;
+    pub fn new(regs: *mut PL011_Regs) -> Self {
+        let regs = unsafe { regs.as_mut() }.unwrap();
+        Self { regs }
     }
 }
